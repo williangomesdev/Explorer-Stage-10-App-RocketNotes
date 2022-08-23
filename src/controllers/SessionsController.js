032fdb9b219ae0b62d9dbd05ecc3af50c966d1b7
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-08-23 10:42:35 -0300

    Validando usuário cadastrado

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,7 +1,21 @@
+//Validação de dados do usuário
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 class SessionsController {
   async create(request, response) {
+    //Buscar informações passadas pelo usurário
     const { email, password } = request.body;
-    return response.json({ email, password });
+
+    //Buscar usuário no banco de dados
+    const user = await knex("users").where({ email }).first();
+
+    //Se usuário não existir informe mensagem de erro
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta", 401);
+    }
+
+    return response.json(user);
   }
 }
 
