a8772d00c0d84c363ccc1a779108309b61589270
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-08-23 11:05:54 -0300

    Validação de senha

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -2,6 +2,9 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 
+//Validando senha criptografada
+const { compare } = require("bcryptjs");
+
 class SessionsController {
   async create(request, response) {
     //Buscar informações passadas pelo usurário
@@ -10,11 +13,18 @@ class SessionsController {
     //Buscar usuário no banco de dados
     const user = await knex("users").where({ email }).first();
 
-    //Se usuário não existir informe mensagem de erro
+    //Se usuário não existir no banco de dados informe mensagem de erro
     if (!user) {
       throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
+  //Comparar senha enviada com a senha do banco
+    const passwordMatched = await compare(password, user.password);
+
+    //Se a senha for diferente da senha cadastrada no banco de dados informe mensagem de erro
+    if(!passwordMatched){
+      throw new AppError("E-mail e/ou senha incorreta", 401);
+    } 
     return response.json(user);
   }
 }
