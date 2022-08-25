38a7fc3606cd33fe87f92ab336c38001051cd074
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-08-25 20:56:17 -0300

    Salvando imagem no banco

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ const uploadConfig = require("../configs/upload");
 
 //importar do controllers
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 //importar middleware
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
@@ -13,19 +14,11 @@ const upload = multer(uploadConfig.MULTER);
 
 //instanciando na memória
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
 //patch atualizar um campo especifico
-usersRoutes.patch(
-  "/avatar",
-  ensureAuthenticated,
-  upload.single("avatar"),
-  (request, response) => {
-    console.log(request.file.filename);
-    response.json();
-  }
-);
-
+usersRoutes.patch("/avatar",ensureAuthenticated,upload.single("avatar"),userAvatarController.update);
 //exportar rotas para o server.js
 module.exports = usersRoutes;
